0de2a30530576f4cc4bff49400a81e8c9ea50868
hjkar1 <hjkar1@outlook.com>
2019-10-25 20:15:52 +0300

    update react-testing-library

--- src/components/Board.test.js
+++ src/components/Board.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Board from './Board';
 import { initializeCards } from '../initialize';
 import images from '../images';
--- src/components/Controls.test.js
+++ src/components/Controls.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Controls from './Controls';
 
 const mockHandler = jest.fn();
